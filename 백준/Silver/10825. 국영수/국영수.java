657ee8c0e910ae572f548e67e3afada275d68f2c
akacongina <62907792+akacongina@users.noreply.github.com>
2023-01-09 22:11:36 +0900

    [Silver IV] Title: 국영수, Time: 716 ms, Memory: 59284 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Student{
+		String name;
+		int kor;
+		int eng;
+		int math;
+		
+		public Student(String name, int kor, int eng, int math) {
+			this.name = name;
+			this.kor = kor;
+			this.eng = eng;
+			this.math = math;
+		}
+	}
+		
+		public static void main(String[] args) throws NumberFormatException, IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer str;
+			
+			ArrayList<Student> list = new ArrayList<>();
+			int n = Integer.parseInt(br.readLine());
+			
+			while(n-- > 0) {
+				str = new StringTokenizer(br.readLine());
+				String name = str.nextToken();
+				int kor = Integer.parseInt(str.nextToken());
+				int eng = Integer.parseInt(str.nextToken());
+				int math = Integer.parseInt(str.nextToken());
+				
+				Student s = new Student(name, kor, eng, math);
+				list.add(s);
+			}
+			
+		Collections.sort(list, new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				if(o1.kor == o2.kor) {
+					if(o1.eng == o2.eng) {
+						if(o1.math == o2.math) {
+							return o1.name.compareTo(o2.name);
+						}
+						return o2.math - o1.math; 
+					}
+					return o1.eng - o2.eng; 
+				}
+				return o2.kor -o1.kor; 
+			}
+			
+		});
+		 
+        StringBuilder sb = new StringBuilder();
+        for (Student student : list) {
+            sb.append(student.name).append('\n');
+        }
+
+        System.out.println(sb);
+			
+		}
+		
+	}
+
